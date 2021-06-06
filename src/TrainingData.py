ba98ec4cea0669f3e7d9ed8577fc595805f05806
Hugo Boullosa <hugo.bou.gz@gmail.com>
2021-06-06 12:09:33 +0200

    Generate training data

---
+++ src/TrainingData.py
@@ -0,0 +1,36 @@
+import random
+import networkx as nx
+import Features as feat
+
+def random_node(graph):
+    return random.choice(list(graph.nodes))
+
+def get_label(graph, src, dst):
+    label = list(map(lambda t:  t[1], graph.out_edges(src))).index(dst)
+    return label
+
+def generate_training_set(graph, n_samples):
+    features = []
+    labels = []
+
+    while len(labels) <= n_samples:
+        src = random_node(graph)
+        dst = random_node(graph)
+        if dst == src:
+            continue
+
+        try:
+            path = nx.astar_path(graph, src, dst)
+        except nx.exception.NetworkXNoPath:
+            continue
+
+        node_pairs = get_node_pairs(path)
+        for (src, dst) in node_pairs:
+            features.append(feat.encode_features(graph, src, dst))
+            labels.append(get_label(graph, src, dst))
+
+    return features, labels
+
+
+def get_node_pairs(path):
+    return [(path[i], path[i + 1]) for i in range(len(path) - 1)]
---
+++ test/TrainingDataTest.py
@@ -0,0 +1,32 @@
+import unittest
+
+import osmnx as ox
+
+import GraphCreator as gc
+import TrainingData as td
+
+class TrainingDataCase(unittest.TestCase):
+    def test_generate_training_set(self):
+        graph = gc.generate_low_degree_g(10)
+        features, labels = td.generate_training_set(graph, 20)
+
+        self.assertGreaterEqual(len(features), 20)
+        self.assertGreaterEqual(len(labels), 20)
+
+        self.assertEqual(len(features[0]), 25)
+
+    def test_generate_training_set_osm_data(self):
+
+        ox.config(use_cache=True, log_console=True)
+
+        graph = ox.graph_from_xml("../data/map.osm")
+
+        features, labels = td.generate_training_set(graph, 20)
+
+        self.assertGreaterEqual(len(features), 20)
+        self.assertGreaterEqual(len(labels), 20)
+
+        self.assertEqual(len(features[0]), 25)
+
+if __name__ == '__main__':
+    unittest.main()
