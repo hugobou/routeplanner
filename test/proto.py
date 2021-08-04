f2d2017655740676d03320d81a8c9b3868987843
Hugo Boullosa <hugo.bou.gz@gmail.com>
2021-08-04 16:08:16 +0200

    Add nx shortest path to proto as reference

--- test/proto.py
+++ test/proto.py
@@ -1,7 +1,8 @@
 import application as app
+import osmnx as ox
+import networkx as nx
 
-
-app = app.application_creator("/home/hugo/PycharmProjects/routeplanner/proto/madrid.gml",
+app = app.application_creator("/home/hugo/PycharmProjects/routeplanner/data/madrid.gml",
                               "/home/hugo/PycharmProjects/routeplanner/test/model_params")
 
 origin_point = (-3.6121729, 40.4224813)
@@ -11,4 +12,13 @@ route, valid = app.get_route(origin_point, destination_point)
 
 print(route)
 print(valid)
+
+G = app.graph
+node_src = ox.distance.nearest_nodes(G, origin_point[0], origin_point[1])
+node_dst = ox.distance.nearest_nodes(G, destination_point[0], destination_point[1])
+route2 = nx.shortest_path(G, node_src, node_dst, weight='length')
+
+print(node_dst)
+print(route2)
+
 
