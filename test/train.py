0982a38ce3df0c79991ffa0d6e93771ad3f180dd
Hugo Boullosa <hugo.bou.gz@gmail.com>
2021-09-13 22:06:42 +0200

    Dirty version save/read training samples

--- test/train.py
+++ test/train.py
@@ -1,5 +1,6 @@
 import traffic
 import mapreader as mr
+import numpy as np
 from model.trainer import train
 from model.model import Model
 from model.modelbuilder import build_model
@@ -12,26 +13,33 @@ import traffic as tf
 def add_weights(G):
     for s, d, meta in G.edges.data():
         for i in range(len(G[s][d])):
-            G[s][d][i]['weight'] = G[s][d][i]['length'] / (G[s][d][i]['speed_kph'] / 3.6 ) * (1 + G[s][d][i]['traffic'])
+            G[s][d][i]['weight'] = G[s][d][i]['length'] / (G[s][d][i]['speed_kph'] / 3.6) * (1 + G[s][d][i]['traffic'])
 
 
 if __name__ == "__main__":
-    graph = mr.ReadMap("../data/madrid.gml")
+    if False:
+        graph = mr.ReadMap("../data/madrid.gml")
 
-    print('update_traffic_info')
-    tm_dict = tf.read_tm_dict('../data/traffic_measurement_points.csv')
-    with open('pm.xml') as xmlfile:
-        tm_list = traffic.parse_traffic_data(xmlfile.read(), debug=False)
-    tf.update_traffic_info(graph, tm_list, tm_dict)
+        print('update_traffic_info')
+        tm_dict = tf.read_tm_dict('../data/traffic_measurement_points.csv')
+        with open('pm.xml') as xmlfile:
+            tm_list = traffic.parse_traffic_data(xmlfile.read(), debug=False)
+        tf.update_traffic_info(graph, tm_list, tm_dict)
 
-    print('add_weights')
-    add_weights(graph)
+        print('add_weights')
+        add_weights(graph)
 
-    print('generate_training_set')
-    X, y = generate_training_set(FeaturesEncoder(), graph, n_samples=15000)
+        print('generate_training_set')
+        X, y = generate_training_set(FeaturesEncoder(), graph, n_samples=20000)
+
+        np.savetxt("x.csv", X, delimiter=",")
+        np.savetxt("y.csv", y, delimiter=",")
+    else:
+        X = np.loadtxt("x.csv", delimiter=",")
+        y = np.loadtxt("y.csv", delimiter=",")
 
     model = Model(build_model())
 
     train(model.get_model(), X, y)
 
-    model.get_model().save_params("../data/model_params")
+    model.get_model().save_params("../data/model_params0")
